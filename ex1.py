f6fc8256f7670680c38ac8f0824d2bdb0a578e73
Samuel Lima <77423761+KAeHM@users.noreply.github.com>
2021-01-14 17:25:46 -0300

    My first tasks =D

--- ex1.py
+++ ex1.py
@@ -3,7 +3,7 @@ numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 matriz = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 for numberl in range(0, 4):
     for numberc in range(0, 4):
-        matriz[numberl][numberc] = random.choice(numbers)
+        matriz[numberl][numberc] = random.choice(numbers) # Manually or not 
 for numberl in range(0, 4):
     for numberc in range(0, 4):
         print(f'[{matriz[numberl][numberc]}]', end = ' ')
